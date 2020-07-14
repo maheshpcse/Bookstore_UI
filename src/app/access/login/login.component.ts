14b7bae613b0d1a3e23ef569aec52bfef601e188
krishnamohanyedla <krishnamohanyedla@gmail.com>
2020-07-14 22:33:40 +0530

    updated

--- src/app/access/login/login.component.ts
+++ src/app/access/login/login.component.ts
@@ -2,8 +2,6 @@ import { AuthService } from './../../auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrManager } from 'ng6-toastr-notifications';
-
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -31,12 +29,22 @@ export class LoginComponent implements OnInit {
     }
     this.authservice.logindetails(obj).subscribe(response => {
       console.log('the response is tee', response);
-      if (response['success'] == true) {
+      if (response['status'] == 200) {
+        sessionStorage.setItem('id', response['id']);
+        sessionStorage.setItem('firstname', response['firstname']);
+        sessionStorage.setItem('firstname', response['lastname']);
+        sessionStorage.setItem('firstname', response['username']);
+        sessionStorage.setItem('firstname', response['password']);
+        sessionStorage.setItem('firstname', response['email']);
+        sessionStorage.setItem('firstname', response['desgination']);
+        sessionStorage.setItem('firstname', response['depertmenet']);
+        sessionStorage.setItem('firstname', response['updated_at']);
         this.toster.successToastr('login  sucess');
         this.router.navigate(['./home'])
       }
       else {
-        this.toster.errorToastr('sign up is failed');
+        this.toster.errorToastr('login is failed');
+        this.router.navigate(['./login'])
       }
     })
   }
